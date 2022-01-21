1a6de56fe08b1cb2d038564f6563d4c0e6852757
x-1337-x <g1thub_acc@protonmail.com>
2022-01-21 22:50:15 +0200

    post api/login

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,8 @@
-const exress = require('express');
+const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const db = require('../db');
-const userRouter = exress.Router();
+const userRouter = express.Router();
 
 userRouter.post('/register', async (req, res, next) => {
 	try {
@@ -25,13 +26,52 @@ userRouter.post('/register', async (req, res, next) => {
 	}
 });
 
-userRouter.get('/getUsers', async (req, res, next) => {
+userRouter.post('/login', async (req, res, next) => {
 	try {
-		let users = await db.query(`select * from Users`);
-		res.send(users);
+		let { email, password } = req.body;
+
+		if (!email || !password) {
+			res.sendStatus(403).send('Check your data');
+			return;
+		}
+
+		const queryResult = await db.query(`select * from users where email=$1`, [
+			email,
+		]);
+		const user = queryResult.rows[0];
+
+		if (!user) {
+			res.sendStatus(403).send('Check your data');
+			return;
+		}
+
+		const match = await bcrypt.compare(password, user.password);
+
+		if (match) {
+			const token = jwt.sign(
+				{ user_id: user.user_id },
+				process.env.JWT_SECRET,
+				{ expiresIn: '6h' }
+			);
+
+			res.json({ token });
+			return;
+		} else {
+			res.sendStatus(403).send('Check your data');
+			return;
+		}
 	} catch (error) {
 		next(error);
 	}
 });
+
+// userRouter.get('/getUsers', async (req, res, next) => {
+// 	try {
+// 		let users = await db.query(`select * from Users`);
+// 		res.send(users);
+// 	} catch (error) {
+// 		next(error);
+// 	}
+// });
 
 module.exports = userRouter;
