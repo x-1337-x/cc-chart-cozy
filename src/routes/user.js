cce2d5e00dcd8905941dfaf0b26edb691a2e35ac
x-1337-x <g1thub_acc@protonmail.com>
2022-01-21 21:55:29 +0200

    installed bcrypt jwt; post api/register

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,25 @@
 const exress = require('express');
+const bcrypt = require('bcrypt');
 const db = require('../db');
 const userRouter = exress.Router();
 
-userRouter.post('/register', (req, res, next) => {
+userRouter.post('/register', async (req, res, next) => {
 	try {
+		let { email, password, repeatPassword } = req.body;
+
+		if (!email || !password || !repeatPassword || password !== repeatPassword) {
+			res.sendStatus(403).send('Check your data');
+		}
+
+		const hashedPassword = await bcrypt.hash(password, 10);
+
+		await db.query(`insert into Users(email, password) values($1, $2)`, [
+			email,
+			hashedPassword,
+		]);
+
+		res.send('User has been added');
+		return;
 	} catch (error) {
 		next(error);
 	}
